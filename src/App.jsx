242669459a5432edaa63344119d274c26f8a9ec1
119kanika <kanika.mahindroo@gmail.com>
2024-06-07 12:23:54 +0530

    css changes and experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 };

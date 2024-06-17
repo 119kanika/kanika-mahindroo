90869e549412531ccaaa18d5f2a58fd5c8577058
119kanika <kanika.mahindroo@gmail.com>
2024-06-17 19:56:40 +0530

    carousel effect on projects section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,15 +1,37 @@
 import projects from "../../data/projects.json";
 import ProjectCard from "./ProjectCard";
 import styles from "./Projects.module.css";
+import { useState } from "react";
+import { FaGreaterThan, FaLessThan } from "react-icons/fa";
 
 const Project = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
+    console.log(projects[currentIndex]);
+  };
+
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+      <div className={styles.carousel}>
+        <button className={styles.buttons} onClick={prevProject}>
+          <FaLessThan />
+        </button>
+        <ProjectCard
+          project={projects[currentIndex]}
+          className={styles.projects}
+        />
+        <button className={styles.buttons} onClick={nextProject}>
+          <FaGreaterThan />
+        </button>
       </div>
     </section>
   );
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -2,12 +2,15 @@
 import { getImageUrl } from "../../utils";
 import styles from "./ProjectCard.module.css";
 
-const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
-}) => {
+const ProjectCard = ({ project }) => {
+  const { imageSrc, title, description, skills, demo, source } = project;
+
   return (
     <div className={styles.container}>
-      <img className={styles.image} src={getImageUrl(imageSrc)} alt={title} />
+      <div
+        className={styles.image}
+        style={{ backgroundImage: `url(${getImageUrl(imageSrc)})` }}
+      ></div>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
